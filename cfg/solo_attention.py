5b72041d1ac30fd45554f3cda2a161d7532fb5f4
dyy0205 <15195762856@163.com>
2020-05-28 19:24:58 +0800

    ADD

--- cfg/solo_attention.py
+++ cfg/solo_attention.py
@@ -1,9 +1,7 @@
-# fp16 settings
-# fp16 = dict(loss_scale=512.)
 # model settings
 model = dict(
     type='SOLO',
-    # pretrained='/versa/dyy/pretrained_models/tf_efficientnet_lite3_tuned.pth',
+    # pretrained='/home/dingyangyang/pretrained_models/tf_efficientnet_lite3_tuned.pth',
     backbone=dict(
         type='EfficientNet_Lite',
         model_name='efficientnet-b3',
@@ -11,13 +9,13 @@ model = dict(
         out_indices=(1, 2, 4, 6),  # C2, C3, C4, C5
         frozen_stages=-1),
     neck=dict(
-        type='FPN',
-        in_channels=[32, 48, 136, 384],
+        type='BiFPN_Lite',  # P2 ~ P6
+        compound_coef=3,
+        num_repeats=1,
         out_channels=160,
-        start_level=0,
-        num_outs=5),
+        freeze_params=False),
     bbox_head=dict(
-        type='DecoupledSOLOLightHead',
+        type='SOLOHead',
         num_classes=5,
         in_channels=160,
         stacked_convs=4,
@@ -27,7 +25,7 @@ model = dict(
         sigma=0.2,
         num_grids=[40, 36, 24, 16, 12],
         cate_down_pos=0,
-        # fp16_training=True,
+        with_attention=True,
         loss_ins=dict(
             type='DiceLoss',
             use_sigmoid=True,
@@ -51,17 +49,16 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/versa/dyy/coco/'
+data_root = '/home/versa/dataset/MSCOCO/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize',
-         img_scale=[(852, 512), (852, 480), (852, 448),
-                    (852, 416), (852, 384), (852, 352)],
+         img_scale=[(832, 512), (832, 448), (832, 384)],
          multiscale_mode='value',
-         keep_ratio=True),
+         keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -72,10 +69,10 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(852, 512),
+        img_scale=(832, 512),
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', keep_ratio=False),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
@@ -84,25 +81,25 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=16,
+    imgs_per_gpu=8,
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'train.json',
-        img_prefix='/versa/dataset/COCO2017/coco/train2017/',
+        img_prefix=data_root + 'train2017/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'val.json',
-        img_prefix=data_root + 'val/',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         ann_file=data_root + 'val.json',
-        img_prefix=data_root + 'val/',
+        img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -121,11 +118,11 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 15
+total_epochs = 12
 device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/decoupled_solo_light_b3_fpn_lite_2'
-load_from = None
-resume_from = './work_dirs/decoupled_solo_light_b3_fpn_lite_2/epoch_5.pth'
+work_dir = './work_dirs/solo_attention'
+load_from = './work_dirs/solo_attention/freeze_backbone_neck_epoch_12.pth'
+resume_from = None
 workflow = [('train', 1)]
