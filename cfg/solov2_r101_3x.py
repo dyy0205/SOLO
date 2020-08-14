d059a01eb441106d97a680320673d852830484c7
dyy0205 <15195762856@163.com>
2020-08-14 11:29:40 +0800

    modify

--- cfg/solov2_r101_3x.py
+++ cfg/solov2_r101_3x.py
@@ -1,20 +1,14 @@
 # model settings
 model = dict(
     type='SOLO',
-    # pretrained='torchvision://resnet101',
+    pretrained='torchvision://resnet101',
     backbone=dict(
         type='ResNet',
         depth=101,
         num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=-1,
-        style='pytorch',
-        # dcn=dict(
-        #     type='DCN',
-        #     deformable_groups=1,
-        #     fallback_on_stride=False),
-        # stage_with_dcn=(False, True, True, True)
-        ),
+        out_indices=(0, 1, 2, 3), # C2, C3, C4, C5
+        frozen_stages=1,
+        style='pytorch'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -23,11 +17,9 @@ model = dict(
         num_outs=5),
     bbox_head=dict(
         type='SOLOV2Head',
-        num_classes=10,
+        num_classes=81,
         in_channels=256,
         stacked_convs=4,
-        use_dcn_in_tower=True,
-        type_dcn='DCN',
         seg_feat_channels=256,
         strides=[8, 8, 16, 32, 32],
         scale_ranges=((1, 96), (48, 192), (96, 384), (192, 768), (384, 2048)),
@@ -38,11 +30,6 @@ model = dict(
             type='DiceLoss',
             use_sigmoid=True,
             loss_weight=3.0),
-        loss_ssim=dict(
-            type='SSIMLoss',
-            window_size=11,
-            size_average=True,
-            loss_weight=2.0),
         loss_cate=dict(
             type='FocalLoss',
             use_sigmoid=True,
@@ -62,19 +49,18 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/versa/dyy/dataset/MSCOCO/'
+data_root = '/home/versa/dataset/MSCOCO/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize',
-         img_scale=[(852, 512), (852, 480), (852, 448),
-                    (852, 416), (852, 384), (852, 352)],
+         img_scale=[(1333, 800), (1333, 768), (1333, 736),
+                    (1333, 704), (1333, 672), (1333, 640)],
          multiscale_mode='value',
          keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    # dict(type='ImgAug', aug_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -84,7 +70,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(852, 512),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -96,49 +82,48 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=8,
-    workers_per_gpu=2,
+    imgs_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'train_9cls_car.json',
+        ann_file=data_root + 'annotations/instances_train2017.json',
         img_prefix=data_root + 'train2017/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'val_9cls.json',
+        ann_file=data_root + 'annotations/instances_val2017.json',
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'val_9cls.json',
+        ann_file=data_root + 'annotations/instances_val2017.json',
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
-    policy='cosine',
+    policy='step',
     warmup='linear',
-    warmup_iters=20000,
+    warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[8, 11])
+    step=[27, 33])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=200,
+    interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         # dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 36
 device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/add_solov2_r101_from_coco_car2'
-load_from = './work_dirs/add_solov2_r101_from_coco_car/epoch_12.pth'
-# load_from = './solov2_r101_imgaug.pth'
+work_dir = './work_dirs/solo_r101_3x'
+load_from = None
 resume_from = None
 workflow = [('train', 1)]
--- inference.py
+++ inference.py
@@ -4,34 +4,34 @@ import os, glob, time
 
 config_file = 'cfg/aug_solov2_r101.py'
 # download the checkpoint from model zoo and put it in `checkpoints/`
-checkpoint_file = './work_dirs/add_solov2_r101_from_coco_car/epoch_12.pth'
+checkpoint_file = './work_dirs/add_solov2_r101_from_coco_car2/epoch_12.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_detector(config_file, checkpoint_file, device='cuda:3')
 # CLASSES = ('person', 'cat', 'dog', 'cartoon', 'horse', 'sheep', 'cow', 'car', 'airplane')
 
-# test a single image
-img = './test1.png'
-result, cost_time = inference_detector(model, img)
-show_result_ins(img, result, model.CLASSES, score_thr=0.2,
-                    out_file='./test1_out.jpeg')
+# # test a single image
+# img = './test1.png'
+# result, cost_time = inference_detector(model, img)
+# show_result_ins(img, result, model.CLASSES, score_thr=0.2,
+#                     out_file='./test1_out.jpeg')
 
-# imgs = glob.glob('./catdog/*.*')
-# # imgs = glob.glob('/home/versa/dataset/MSCOCO/aug_seg/val_imgs/*.*')
-# save_dir = './catdog_out'
-# if not os.path.exists(save_dir):
-#     os.makedirs(save_dir)
-#
-# total = 0
-# for i, img in enumerate(imgs):
-#     name = img.split('/')[-1]
-#     if not name.endswith('HEIC'):
-#         result, cost_time = inference_detector(model, img)
-#         print(i, name, cost_time)
-#         total += cost_time
-#         try:
-#             show_result_ins(img, result, model.CLASSES, score_thr=0.2,
-#                         out_file=os.path.join(save_dir, name))
-#         except:
-#             continue
-# print('average cost time: ', total / len(imgs))
+imgs = glob.glob('./test_add_imgs/*.*')
+# imgs = glob.glob('/home/versa/dataset/MSCOCO/aug_seg/val_imgs/*.*')
+save_dir = './add_out'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+total = 0
+for i, img in enumerate(imgs):
+    name = img.split('/')[-1]
+    if not name.endswith('HEIC'):
+        result, cost_time = inference_detector(model, img)
+        print(i, name, cost_time)
+        total += cost_time
+        try:
+            show_result_ins(img, result, model.CLASSES, score_thr=0.25,
+                        out_file=os.path.join(save_dir, name))
+        except:
+            continue
+print('average cost time: ', total / len(imgs))
